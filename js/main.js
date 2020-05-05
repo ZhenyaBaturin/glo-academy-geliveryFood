c21ba4bc0c9f01b8f441d02c52f3b60158de9236
Baturin <zhenyabaturin21.91@gmail.com>
2020-05-05 17:27:24 +0500

    first day + js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-const cartButton = document.getElementById('cart-button');
-const modal = document.querySelector(".modal"),
-    close = document.querySelector('.close')
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
-cartButton.addEventListener('click', toggleModal);
-close.addEventListener('click', toggleModal);
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
 
 function toggleModal() {
-    modal.classList.toggle('is-open');
-};
-// активизация WOW
-new.WOW().init();
+  modal.classList.toggle("is-open");
+}
